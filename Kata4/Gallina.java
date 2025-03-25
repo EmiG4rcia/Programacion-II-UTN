616fafbcc37f2cfe4b0d7c35adc61a0e92368478
EmiG4rcia <emi.almagro.2001@gmail.com>
2025-03-24 23:33:38 -0300

    Corrección de clase Gallina

--- Kata4/Gallina.java
+++ Kata4/Gallina.java
@@ -1,12 +1,15 @@
 package practico_3.Kata4;
 
 public class Gallina {
-    static int idGallina=0;
+    private static int idContador=0;
+    private int idGallina;
     private int edad;
     private int huevosPuestos;
 
     public Gallina (int edad, int huevosPuestos){
-        idGallina++;
+        idContador++;
+        //this.idContador=idContador++;
+        this.idGallina=idContador;
         this.edad=edad;
         this.huevosPuestos=huevosPuestos;
     }
